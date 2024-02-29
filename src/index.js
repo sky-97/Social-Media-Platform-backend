1d28a0738c7d50ae9875f487debbc1dd34ecb02f
Akash Shridhar Shetty <as@softxways.com>
2024-02-29 18:03:35 +0530

    connected mongo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-var name 
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './.env'
+})
+
+
+ 
+connectDB()
